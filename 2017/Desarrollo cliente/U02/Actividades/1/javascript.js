a7cf14592e9cd0a7b98afcc663496e1d9465f844
Emiliano Montesdeoca del Puerto <emontesdeoc@gmail.com>
2017-09-21 12:16:28 +0100

    Stuff

--- 2017/Desarrollo cliente/U02/Actividades/1/javascript.js
+++ 2017/Desarrollo cliente/U02/Actividades/1/javascript.js
@@ -1,14 +1,36 @@
 function tellFortune(children, fname, loc, work) {
-  alert("Seras " + work + " en " + loc + ", estaras casado con " + fname + " y tendras "+children + " hijos.")
+  alert("Seras " + work + " en " + loc + ", estaras casado con " + fname + " y tendras " + children + " hijos.")
 }
 
-/*tellFortune(4,"Emiliano", "Uruguay", "Programador de PHP");
-tellFortune(5,"Messi", "Argentina", "Pelotuda");
-tellFortune(1,"Yaco", "Tenerife", "Programador de Java");*/
-
-
-function calculateAge(a,b) {
-  alert("Estas entre " + b-a-1 + " o " + b-a + " años.")
+function calculateAge(a, b) {
+  alert("Estas entre " + b - a - 1 + " o " + b - a + " años.")
 }
 
-var date = new Date();
+function isLeapYear(year) {
+  return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
+
+const emax = 100;
+
+function calculateSupplyFlotante(a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return console.log("Error en datos");
+  }
+
+  var yearToLive = emax - a;
+  var currentYear = new Date().getFullYear();
+  var lastYear = currentYear + yearToLive;
+  var counter = 0;
+
+  for (var i = currentYear; i <= lastYear; i++) {
+    if (isLeapYear(currentYear)) {
+      counter += b * 366;
+    } else {
+      counter += b * 365;
+    }
+  }
+
+  return console.log("Necesitaras " + counter.toFixed() + " hasta que tengas " + emax + " años y te vas a gastar " + (0.80 * counter).toFixed(2) + "€.");
+}
+
+calculateSupplyFlotante(22, 2.8);
--- 2017/Desarrollo servidor/T1/AUTO01_01_MontesdeocaEmiliano/1/src/pkg1/Main.java
+++ 2017/Desarrollo servidor/T1/AUTO01_01_MontesdeocaEmiliano/1/src/pkg1/Main.java
@@ -1,42 +1,26 @@
 package pkg1;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         //Emiliano Montesdeoca del Puerto
-        System.out.print("Primer numero: ");
-        int a = new Scanner(System.in).nextInt();
-        System.out.print("Segundo numero: ");
-        int b = new Scanner(System.in).nextInt();
+        /*Persona p1 = new Persona("Marta", "Sánchez", 42);
+        Persona p2 = new Persona("Juan", "Diego", 39);
+
+        mostrarDatosPersona(p1);
         System.out.println("");
-        System.out.println("Suma: " + Suma(a,b));
-        System.out.println("Resta: " + Resta(a,b));
-        System.out.println("Division: " + Division(a,b));
-        System.out.println("Modulo: " + Modulo(a,b));
-     
+        mostrarDatosPersona(p2);*/
+        System.out.println(Integer.parseInt(args[0])*100);
     }
-    
-    public static int Suma(int a, int b){
-        return a+b;
+
+    public static void mostrarDatosPersona(Persona p) {
+
+        System.out.println("Nombre: " + p.nombre);
+        System.out.println("Apellido: " + p.apellidos);
+        System.out.println("Edad: " + p.edad);
+
     }
-    public static int Resta(int a, int b){
-        return a-b;
-    }
-    public static int Division(int a, int b){
-        return a/b;
-    }
-    public static int Modulo(int a, int b){
-        return a%b;
-    }
-    
-    public static String Ejercicio1(){
-        return "EL SYSTEM_ROOT de Windows está ubicado en: \nC:\\Windows\\System32\\";
-    }
-    public static String Ejercicio2(){
-        return "En un lugar de la \"La Mancha\" de cuyo nombre no quiero acordarme \nVivía un 'Hidalgo'";
-    }
-    public static String Ejercicio3(){
-        return "Procesador \t Fabricante \nPentiuim \t Intel \nAthlon \t\t AMD";
-    }
+
 }

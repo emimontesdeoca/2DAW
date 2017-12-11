ac7a2d07b8b28cb9a8f353ba19bbadf34fd396c1
Emiliano Montesdeoca <emontesdeoc@gmail.com>
2017-12-11 13:45:14 +0000

    DSW

--- 2017/DSW/Segundo trimestre/Practicas/AUT06_01_Montesdeoca_DelPuerto_Emiliano_SakilaJSF/SakilaJSF_Emiliano/src/main/java/es/cifpcm/sakilajsf_emiliano/bean/ActorBean.java
+++ 2017/DSW/Segundo trimestre/Practicas/AUT06_01_Montesdeoca_DelPuerto_Emiliano_SakilaJSF/SakilaJSF_Emiliano/src/main/java/es/cifpcm/sakilajsf_emiliano/bean/ActorBean.java
@@ -6,6 +6,11 @@ import es.cifpcm.sakilajsf_emiliano.pojo.Actor;
 import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.FacesComponent;
+import javax.faces.context.FacesContext;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  *
@@ -14,6 +19,24 @@ import javax.enterprise.context.RequestScoped;
 @Named(value = "actorBean")
 @RequestScoped
 public class ActorBean extends Actor {
+
+    @NotNull(message = "Apellidos no puede estar vacío")
+    @Size(min = 1, message = "Apellido no puede estar vacio")
+    @Override
+    public String getLastName() {
+        return super.getLastName();
+    }
+
+    @NotNull(message = "Nombre no puede estar vacío")
+    @Size(min = 1, message = "Nombre no puede estar vacio")
+    @Override
+    public String getFirstName() {
+        return super.getFirstName();
+    }
+
+    public void error() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Chacho la base de dato no funciona, mira ver oiste!"));
+    }
 
     public List<Actor> getActorList() {
         ActorDao a = new ActorDaoImplements();
@@ -24,10 +47,15 @@ public class ActorBean extends Actor {
 
     public String save() {
         actor = new ActorDaoImplements().insert(this);
-        return "actorDetail.xhtml?faces-redirect=true";
+        if (actor.getActorId() == null) {
+            return "/error.xhtml?faces-redirect=true";
+        } else {
+            return "actorDetail.xhtml?faces-redirect=true";
+        }
     }
 
     public static Actor getActor() {
         return actor;
     }
+
 }

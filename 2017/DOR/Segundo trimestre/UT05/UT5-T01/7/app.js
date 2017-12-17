9addacbd524fcffebecb480c53c9d928fd5e1434
Emiliano Montesdeoca <emontesdeoc@gmail.com>
2017-12-17 18:12:24 +0000

    DOR

--- 2017/DOR/Segundo trimestre/UT05/UT5-T01/7/app.js
+++ 2017/DOR/Segundo trimestre/UT05/UT5-T01/7/app.js
@@ -4,22 +4,34 @@ console.log(a);
 for (let index = 0; index < a.length; index++) {
   const element = a[index];
   element.addEventListener("mouseover", checkedStar, false);
-  element.addEventListener("mouseout", uncheckStar, false);
+  element.addEventListener("click", checkedStar, false);
+  // element.addEventListener("mouseout", uncheckStar, false);
 }
 
 function checkedStar(e) {
+  uncheckStar();
   let current = parseInt(e.srcElement.getAttribute("pos"));
 
   for (let index = 0; index < current; index++) {
     const element = a[index];
-    element.className = "fa fa-star checked";
+    element.className = "fa fa-star stars checked";
   }
 }
 
 function uncheckStar() {
-
   for (let index = 0; index < a.length; index++) {
     const element = a[index];
-    element.className = "fa fa-star unchecked";
+    element.className = "fa fa-star stars unchecked";
   }
 }
+
+function setValue(e) {
+  let current = parseInt(e.srcElement.getAttribute("pos"));
+
+  document.getElementById("value").value = current;
+}
+
+// var stars = document.getElementsByTagName("span");
+a.forEach(element => {
+  element.addEventListener("click", setValue, false);
+});

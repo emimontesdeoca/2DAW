a73caaf591582aa3b81c75310522c333daec9985
Emiliano Montesdeoca <emontesdeoc@gmail.com>
2017-12-17 23:11:40 +0000

    DOR

---
+++ 2017/DOR/Segundo trimestre/UT05/UT5-T01/6/app.js
@@ -0,0 +1,8 @@
+function abrir() {
+  var x = document.querySelectorAll(".mensaje");
+  x[0].style.marginLeft = "0%";
+}
+function cerrar() {
+  var x = document.querySelectorAll(".mensaje");
+  x[0].style.marginLeft = "-100%";
+}

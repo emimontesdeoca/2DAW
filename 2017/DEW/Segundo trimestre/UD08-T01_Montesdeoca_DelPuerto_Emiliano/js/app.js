00bb63a1e68408c10103e6971c42a45ba2fec889
Emiliano Montesdeoca <emontesdeoc@gmail.com>
2018-01-18 10:40:04 +0000

    DEW

--- 2017/DEW/Segundo trimestre/UD08-T01_Montesdeoca_DelPuerto_Emiliano/js/app.js
+++ 2017/DEW/Segundo trimestre/UD08-T01_Montesdeoca_DelPuerto_Emiliano/js/app.js
@@ -1,5 +1,6 @@
 var $gallery = $("#gallery");
-
+var numeroIslasValue = 4;
+var nombreJugador = "Jugador";
 var arrayIslas = [
   ($lapalma = $("#Lapalma")),
   ($elhierro = $("#Elhierro")),
@@ -25,23 +26,38 @@ var arrayIslasCrear = [
   { id: 6, name: "Lanzarote", title: "Lanzarote", att: "lanzarote" }
 ];
 
-$(document).ready(function() {
+$(document).ready();
+
+function jugar() {
   // Contenedores
   var c = document.getElementById("gallery");
+  /// Borrar lo que tiene dentro
+  while (c.firstChild) {
+    c.removeChild(c.firstChild);
+  }
+
   var ci = document.getElementById("islas");
+  /// Borrar lo que tiene dentro
+  while (ci.firstChild) {
+    ci.removeChild(ci.firstChild);
+  }
 
   // Numero random de islas
-  var random = Math.floor(Math.random() * (6 - 3 + 1)) + 3;
+  var numeroIslas = parseInt(numeroIslasValue);
+  var numeroImagenes = numeroIslas + 2;
+  var imagenesRestantes = numeroImagenes - numeroIslas;
   var aIslas = [];
 
   // Crear ids para las islas, que nunca se repitan
   var arr = [];
-  while (arr.length < random) {
+  while (arr.length < numeroIslas) {
     var randomnumber = Math.floor(Math.random() * (6 - 0 + 1)) + 0;
     if (arr.indexOf(randomnumber) > -1) continue;
     arr[arr.length] = randomnumber;
   }
 
+  /// Randomizar el array
+  shuffle(arrayIslasCrear);
   /// Utilizando el array de islas agregar dependiendo el id generado en el arr
   arr.forEach(element => {
     aIslas.push(arrayIslasCrear[element]);
@@ -52,10 +68,7 @@ $(document).ready(function() {
     let div = document.createElement("div");
     div.setAttribute("id", element.name);
     div.setAttribute("isla", element.att);
-    div.setAttribute(
-      "class",
-      "contenedor-imgs ui-widget-content ui-state-default"
-    );
+    div.setAttribute("class", "contenedor-imgs ");
 
     let h4 = document.createElement("h4");
     h4.setAttribute("class", "ui-widget-header");
@@ -66,25 +79,82 @@ $(document).ready(function() {
     ci.appendChild(div);
   });
 
-  console.log(aIslas);
+  // Conseguir imagenes teniendo las islas ya
+  var arrayIslasNombre = [];
 
+  aIslas.forEach(element => {
+    var a = { title: element.title, att: element.att };
+    arrayIslasNombre.push(a);
+  });
+
+  shuffle(arrayIslasNombre);
+
+  var arrayIslasFinal = [];
+  var finalArray = [];
+
+  /// Una imagen por cada isla
+  for (let index = 0; index < arrayIslasNombre.length; index++) {
+    const element = arrayIslasNombre[index];
+    var imagenumber = Math.floor(Math.random() * (4 - 0) + 0);
+    arrayIslasFinal.push(imagenumber);
+  }
+
+  for (let index = 0; index < arrayIslasFinal.length; index++) {
+    var att = arrayIslasNombre[index].att;
+    var title = arrayIslasNombre[index].title;
+    var imgN = arrayIslasFinal[index];
+    var path = "images/" + att + "/" + imgN + ".jpg";
+
+    var obj = { att: att, title: title, path: path };
+
+    if (finalArray.indexOf(obj) == -1) {
+      finalArray.push(obj);
+    } else {
+      index--;
+    }
+  }
+
+  // Imagenes retantes
+  arrayIslasFinal = [];
+  for (let index = 0; index < imagenesRestantes; index++) {
+    const element = arrayIslasNombre[index];
+    var imagenumber = Math.floor(Math.random() * (4 - 0) + 0);
+    arrayIslasFinal.push(imagenumber);
+  }
+
+  for (let index = 0; index < imagenesRestantes; index++) {
+    var att = arrayIslasNombre[index].att;
+    var title = arrayIslasNombre[index].title;
+    var imgN = arrayIslasFinal[index];
+    var path = "images/" + att + "/" + imgN + ".jpg";
+
+    var obj = { att: att, title: title, path: path };
+
+    if (finalArray.indexOf(obj) == -1) {
+      finalArray.push(obj);
+    } else {
+      index--;
+    }
+  }
+
+  // shuffle(finalArray);
   // Crear las imagenes
-  for (let index = 0; index < 5; index++) {
+  finalArray.forEach(element => {
     var li = document.createElement("li");
     li.setAttribute(
       "class",
       "ui-widget-content ui-corner-tr ui-draggable ui-draggable-handle"
     );
-    li.setAttribute("isla", "elhierro");
+    li.setAttribute("isla", element.att);
 
     var h5 = document.createElement("h5");
     h5.setAttribute("class", "ui-widget-header");
-    h5.innerHTML = "elhierro";
+    h5.innerHTML = element.title;
 
     li.appendChild(h5);
 
     var img = document.createElement("img");
-    img.setAttribute("src", "images/elhierro/uno.jpg");
+    img.setAttribute("src", element.path);
     img.setAttribute("width", "96");
     img.setAttribute("height", "72");
     img.setAttribute("alt", "hola");
@@ -92,7 +162,7 @@ $(document).ready(function() {
     li.appendChild(img);
 
     c.appendChild(li);
-  }
+  });
 
   $("li", $gallery).draggable({
     cancel: "a.ui-icon", // clicking an icon won't initiate dragging
@@ -135,4 +205,52 @@ $(document).ready(function() {
       });
     });
   }
+
+  function dynamicSort(property) {
+    var sortOrder = 1;
+    if (property[0] === "-") {
+      sortOrder = -1;
+      property = property.substr(1);
+    }
+    return function(a, b) {
+      var result =
+        a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+      return result * sortOrder;
+    };
+  }
+
+  function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+
+  var cT = document.getElementById("contenedor-todo");
+  cT.setAttribute("style", "display:inline");
+}
+
+$(function() {
+  $("#dialog-confirm").dialog({
+    resizable: false,
+    height: "auto",
+    width: 400,
+    modal: true,
+    buttons: {
+      Jugar: function() {
+        $(this).dialog("close");
+        var a = document.getElementById("dificultad").value;
+        numeroIslasValue = parseInt(a);
+
+        var b = document.getElementById("username").value;
+        nombreJugador = b;
+
+        var c = document.getElementById("nombreJugador");
+        c.innerHTML = nombreJugador;
+        jugar();
+      }
+    }
+  });
 });
